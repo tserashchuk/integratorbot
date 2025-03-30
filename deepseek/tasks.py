aba7cf4732d2492df99b54f54299968e19f78a53
tserashchuk <tserashchuk.kirill@gmail.com>
2025-03-30 13:12:09 +0300

    л

--- deepseek/tasks.py
+++ deepseek/tasks.py
@@ -16,7 +16,7 @@ def addf(user):
   
    data = bx24.callMethod('crm.deal.list',
                   filter={'CLOSED': 'N',
-                           '>DATE_CREATE':'2025-03-23'},
+                           '>DATE_CREATE':'2025-03-25'},
                   select=['ID', 'TITLE', 'STAGE_ID','DATE_CREATE','DATE_MODIFY','CREATED_BY_ID','ASSIGNED_BY_ID','OPPORTUNITY','IS_RETURN_CUSTOMER','CONTACT_ID','CATEGORY_ID'])
    
 
